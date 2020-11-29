e092a81fbfc453637a76a2f3a2aadf846bbd2d98
Kevin Hudgens <hudgens1073@gmail.com>
2020-11-29 13:54:20 +0900

    Created a path that displays the Detail View and the Create View

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, PostCreateView
 from . import views
 
 urlpatterns = [
+    # The home page now shows all of the posts according to the list view
     path('', PostListView.as_view(), name='blog-home'),
+    # created a path that uses primary key to route the individual data entries
     path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
+    # Now includes a view to create posts
+    path('post/new', PostCreateView.as_view(), name='post-create'),
     path('about/', views.about, name='blog-about'), 
 
 ]
